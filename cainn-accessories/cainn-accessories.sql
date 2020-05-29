93e94356575b68c7f46d2fc78d4a34a0db78b875
cainnesx <65621506+cainnesx@users.noreply.github.com>
2020-05-29 20:59:11 +0100

    Add files via upload

---
+++ cainn-accessories/cainn-accessories.sql
@@ -0,0 +1,6 @@
+INSERT INTO `datastore` (name, label, shared) VALUES
+	('user_ears', 'Orelhas', 0),
+	('user_glasses', 'Óculos', 0),
+	('user_helmet', 'Capacete', 0),
+	('user_mask', 'Mascara', 0)
+;
